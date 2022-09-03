422f0fd39fbf61c8709991c2767ee77642e1e7a1
ijlik <ijlik.k5s@gmail.com>
2022-09-03 10:22:19 +0700

    Fix report Sales Product by filter

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -38,7 +38,8 @@ class ReportController extends Controller
         return view('admin.report.index', compact('orders', 'totalComplete', 'totalPending'));
     }
 
-    public function reports(Request $request){
+    public function reports(Request $request)
+    {
         // Gain loss
         if (isset($request['start_date']) && isset($request['end_date'])) {
             $orders = Order::where('status', 'done')
@@ -53,36 +54,47 @@ class ReportController extends Controller
         $omset = 0;
         $totalAmount = 0;
         $untungRugi = 0;
-        foreach($orders as $order){
+        foreach ($orders as $order) {
             $modal += $order->modal();
             $omset += $order->amount();
             $totalAmount += $order['total_weight'];
         }
-        $untungRugi = ($omset-$modal)*(100-0.25)/100;
+        $untungRugi = ($omset - $modal) * (100 - 0.25) / 100;
 
         $product = Product::all();
         $resultProduct = [];
         foreach ($product as $item) {
             $subTotal = 0;
             foreach ($item->productDetails()->get() as $pdl) {
-                $subTotal += $pdl->orderDetails()->sum('amount');
+                $orderDetails = $pdl->orderDetails()->load('order');
+                $subTotal += $orderDetails->whereHas('order', function ($order) use ($request) {
+                    if (isset($request['start_date']) && isset($request['end_date'])) {
+                        $order->where('status', 'done')
+                            ->whereDate('created_at', '>=', $request['start_date'])
+                            ->whereDate('created_at', '<=', $request['end_date']);
+                    } else {
+                        $order->where('status', 'done');
+                    }
+                })->sum('amount');
             }
 
             $resultProduct [] = [
-                'name'=>$item->name,
-                'subTotal'=> $subTotal
+                'name' => $item->name,
+                'subTotal' => $subTotal
             ];
         }
-        return view('admin.report.reports',compact('modal','omset','untungRugi','orders','totalAmount','resultProduct'));
+        return view('admin.report.reports', compact('modal', 'omset', 'untungRugi', 'orders', 'totalAmount', 'resultProduct'));
     }
 
-    public function stocksHistory(){
+    public function stocksHistory()
+    {
         $historyStocks = HistoryStock::with('productDetail.product')->get();
 
         return view('admin.report.stock', compact('historyStocks'));
     }
 
-    public function pricesHistory(){
+    public function pricesHistory()
+    {
         $historyPrices = HistoryPrice::with('productDetail.product')->get();
 
         return view('admin.report.price', compact('historyPrices'));
